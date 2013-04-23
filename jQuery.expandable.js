c08de423cd61a00c65be945820f7068bb3779065
Bo Frederiksen <bofrede@bofrede.dk>
2013-04-23 23:03:51 +0200

    JsLint free.

--- jQuery.expandable.js
+++ jQuery.expandable.js
@@ -1,22 +1,22 @@
-/**
- * Copyright 2008 BoFrederiksen/BoFrede ApS
- */
+/*jslint sloppy: true */
+/*global jQuery: false, document: false */
+/* Copyright 2008-2013 BoFrederiksen. */
 
 jQuery(document).ready(function () {
-	function toggle(elm) {
-		jQuery(this).next().toggle();
-		if ('none' === jQuery(this).next().css('display')) {
-			jQuery(this).removeClass('collapsable');
-			jQuery(this).addClass('expandable');
-		} else {
-			jQuery(this).removeClass('expandable');
-			jQuery(this).addClass('collapsable');
-		}
-	}
-	var exps = jQuery('.expandable');
-	exps.next().hide();
-	exps.click(toggle);
-	var cols = jQuery('.collapsable');
-	cols.next().show();
-	cols.click(toggle);
+    function toggle() {
+        jQuery(this).next().toggle();
+        if ('none' === jQuery(this).next().css('display')) {
+            jQuery(this).removeClass('collapsable');
+            jQuery(this).addClass('expandable');
+        } else {
+            jQuery(this).removeClass('expandable');
+            jQuery(this).addClass('collapsable');
+        }
+    }
+    var exps = jQuery('.expandable'),
+        cols = jQuery('.collapsable');
+    exps.next().hide();
+    exps.click(toggle);
+    cols.next().show();
+    cols.click(toggle);
 });
